474893ae0292d704cfd0304b9cea834a020f9d44
David Henshaw <davidhenshaw2014@gmail.com>
2020-12-12 08:21:16 -0600

    Add Puzzle Test Scene

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    int id;
+    Vector3 initPos;
+    bool isOpen;
+    [SerializeField] float openDistance = 2;
+    [SerializeField] float openTime = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Open")]
+    public void Open()
+    {
+        //Tween up
+        if(!isOpen)
+        {
+            transform.DOMoveY(initPos.y + openDistance, openTime, false).SetEase(Ease.InOutQuad);
+            isOpen = true;
+        }
+
+    }
+
+    [ContextMenu("Close")]
+    public void Close()
+    {
+        //Tween Down
+        if(isOpen)
+        {
+            transform.DOMoveY(initPos.y, openTime, false).SetEase(Ease.InOutQuad);
+            isOpen = false;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    [SerializeField] UnityEvent[] OnActivate;
+    [SerializeField] UnityEvent[] OnDeactivate;
+    bool isDown = false;
+
+    public void Activate()
+    {
+        foreach(UnityEvent ue in OnActivate)
+        {
+            ue?.Invoke();
+        }
+    }
+
+    public void Deactivate()
+    {
+        foreach (UnityEvent ue in OnDeactivate)
+        {
+            ue?.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isDown)
+        {
+            Activate();
+            isDown = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(isDown)
+        {
+            Deactivate();
+            isDown = false;
+        }
+    }
+}
